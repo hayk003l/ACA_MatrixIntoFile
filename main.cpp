d87e5ffb9ceaf7c17d9d02e1ffd02a665653e2fe
hayk003l <haykharutyunyan113@gmail.com>
2024-05-24 20:32:34 +0400

    3.0

--- main.cpp
+++ main.cpp
@@ -2,14 +2,33 @@
 
 
 int main() {
-    Matrix obj(3, 4);
-    obj.fillMatrix();
+    Matrix objOne(3, 4);
+    objOne.fillMatrix();
 
-    obj.writeMatrixToFile("matrixOne.txt");
+    objOne.writeMatrixToFile("matrixOne.txt");
 
-    Matrix obj2(3, 4);
+    Matrix objTwo(4, 3);
+    objTwo.fillMatrix();
+
+    objTwo.writeMatrixToFile("matrixTwo.txt");
+
+    Matrix obj1(3, 4);
+    obj1.fillMatrixFromFile("matrixOne.txt");
+    obj1.printMatrix();
+
+    std::cout << std::endl;
+
+    Matrix obj2(4, 3);
     obj2.fillMatrixFromFile("matrixOne.txt");
     obj2.printMatrix();
+
+    std::cout << std::endl;
+    
+    Matrix obj3(3, 3);
+
+    obj1.matrixMul(obj2, obj3);
+    obj3.printMatrix();
+    obj3.writeMatrixToFile("matrixThree.txt");
 
     return 0;
 }
